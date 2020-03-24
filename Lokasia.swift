9ae0a7210a2da99ec6083a471640161927cee56a
Faraz Ahmed <khatrifaraz40@gmail.com>
2020-03-24 21:31:40 +0500

    initial commit

---
+++ Lokasia.swift
@@ -0,0 +1,4 @@
+
+// DOWNLOAD THIS APP FROM APPLE STORE
+
+https://apps.apple.com/us/app/lokasia/id1477699739
--- TikBooster.swift
+++
@@ -1,4 +0,0 @@
-
-// DOWNLOAD THIS APP FROM APPLE STORE
-
-https://apps.apple.com/my/app/tikbooster-fans-likes-stats/id1503518100
